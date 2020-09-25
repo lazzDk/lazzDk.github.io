85741d89fa10f677b0219e4b431731dca7818109
Lasse Andreasen <unterhugo@gmail.com>
2020-09-26 01:36:07 +0200

    Fixed fart-angle-calculation

--- fartGame/index.js
+++ fartGame/index.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
     var buttObject;
     var score = 0;
-
+    var imageAngle = 0;
     $('#butt').click(function(e) { 
         score++;
         $('#score').text(score);
@@ -21,7 +21,8 @@ $(document).ready(function() {
         
         var currentPosition = buttObject.position();
         var angle = getAngle(currentPosition.left, currentPosition.top, newPosition[1], newPosition[0])
-        
+        imageAngle = angle;
+
         buttObject.animate(
             { deg: angle },
             {
@@ -31,7 +32,6 @@ $(document).ready(function() {
               }
             } 
         );
-
         var randomSound = Math.round(Math.random() * 3) + 1; 
         var audioFart = $('audio#fart' + randomSound)[0];
         buttObject.animate(
@@ -50,7 +50,7 @@ $(document).ready(function() {
                     toggleButtImage(true);
                     handleFartEffect(currentPosition, true);
                 }
-        });
+        }); 
     });
     
     function toggleButtImage(done){
@@ -64,7 +64,7 @@ $(document).ready(function() {
         }
 
         var setFlame = $('#flameCheckbox').is(':checked');
-        if(setFlame) {
+        if(!setFlame) {
             $('.flame').toggle();
         }
     };
@@ -79,10 +79,42 @@ $(document).ready(function() {
     };
 
     function addFartPattern(currentTop, currentLeft) {
+        var centerTop = currentTop + 100;
+        var centerLeft = currentLeft + 90;
 
+        var adjustedHypLength = 120;
+        console.log(imageAngle);
+
+        var adjustedAngle = Math.abs(imageAngle);
+        adjustedAngle = adjustedAngle > 90 ?  180 - adjustedAngle : adjustedAngle;
+        var xLength = Math.abs(Math.cos(adjustedAngle) * adjustedHypLength);
+        var yLength = Math.abs(Math.sqrt(Math.pow(adjustedHypLength, 2)-Math.pow(xLength, 2)));
+        if(imageAngle >= 90 && imageAngle < 180) {
+            logEvent(90, 180, xLength, yLength);
+            centerTop -= yLength;
+            centerLeft += xLength;
+        } else if(imageAngle >= 0 && imageAngle < 90 ) {
+            logEvent(0, 90, xLength, yLength);
+            centerTop += yLength;
+            centerLeft -= xLength;
+        } else if(imageAngle >= -90 && imageAngle < 0) {
+            logEvent(-90, 0, xLength, yLength);
+            centerTop += yLength;
+            centerLeft -= xLength;
+        } else if(imageAngle >= -180 && imageAngle < -90) {
+            logEvent(-180, -90, xLength, yLength);
+            centerTop -= yLength;
+            centerLeft += xLength;
+        } 
         var img = $('<img class="fart-pattern">');
         img.attr('src', 'assets/splat.svg');
-        img.css({top: currentTop + 100, left: currentLeft});
+        img.css({top: centerTop, left: centerLeft});
         img.appendTo('.container');
     }
+
+    function logEvent(angle1, angle2, xLength, yLength) {
+        return;
+        console.log(">= " + angle1 + " - < " + angle2 + " - x: " + xLength.toFixed(2) + " y: " +yLength.toFixed(2))
+    }
+
 });
